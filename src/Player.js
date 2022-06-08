7dc833bce8ca2268d0eab84ec6e1f8621d8000cd
Anthony Shellman <atshellman@gmail.com>
2022-06-08 11:54:16 -0600

    Add constructor and method to Player class

---
+++ src/Player.js
@@ -0,0 +1,18 @@
+class Player {
+  constructor(name, token, wins, losses, drawGames) {
+    this.name = name;
+    this.token = token;
+    this.wins = wins;
+    this.losses = losses;
+    this.drawGames = drawGames;
+  }
+
+  takeTurn() {
+
+  }
+}
+
+
+
+
+module.exports = Player;
