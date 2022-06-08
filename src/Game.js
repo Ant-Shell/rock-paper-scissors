c6d488e5f36c2767c763ddf610d6f8984200ada5
Anthony Shellman <atshellman@gmail.com>
2022-06-08 17:48:13 -0600

    Create constructor, add potential methods

--- src/Game.js
+++ src/Game.js
@@ -1,11 +1,23 @@
+var Player = require("./Player.js");
+
 class Game {
-  constructor() {
+  constructor(playerDetails) {
+    this.playerName = playerDetails.name;
+    this.playerAvatar = playerDetails.avatar;
+    this.playerWins = playerDetails.wins;
+    this.playerLosses = playerDetails.losses;
+    this.playerDrawGames = playerDetails.drawGames;
+    this.Traditional = false;
+    this.Extreme = false;
+  }
+
+  select() {
+
+  }
+
+  setup() {
 
   }
 }
-
-
-
-
 
 module.exports = Game;
