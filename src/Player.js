0a72cc811c84c6c9223cdbe2a2d3da72f5202255
Anthony Shellman <atshellman@gmail.com>
2022-06-09 10:13:11 -0600

    Modify constructor for testing

--- src/Player.js
+++ src/Player.js
@@ -1,14 +1,17 @@
 class Player {
-  constructor(name, avatar, wins, losses, drawGames) {
+  constructor(name)) {
     this.name = name;
-    this.avatar = avatar;
-    this.wins = wins;
-    this.losses = losses;
-    this.drawGames = drawGames;
+    this.avatar = null;
+    this.wins = 0;
+    this.losses = 0;
+    this.drawGames = 0;
   }
 
   chooseAvatar() {
-
+    var avatarList = [avatar1, avatar2, avatar3, avatar4, avatar5]
+    for (var i = 0; i < avatarList.length; i++) {
+      console.log(avatarList[i])
+    }
   }
 
   chooseGame() {
