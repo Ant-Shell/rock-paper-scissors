dbcdbf03caddcd7d7b7527746f46be55d49cd58c
Anthony Shellman <atshellman@gmail.com>
2022-06-10 16:02:07 -0600

    Add potential arrays for data

---
+++ src/data.js
@@ -0,0 +1,24 @@
+// Assets
+var player1Avatars = [
+
+];
+
+// Assets
+var player2Avatars = [
+
+];
+
+// Strings
+var gameWinQuotes = [
+
+];
+
+// Strings
+var gameLossQuotes = [
+
+];
+
+// Strings
+var gameTieQuotes = [
+
+];
