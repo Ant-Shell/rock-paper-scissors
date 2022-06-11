82b5e38a49de99b2b0ad14a4e442fb6eb5e168d6
Anthony Shellman <atshellman@gmail.com>
2022-06-10 18:23:03 -0600

    Add values for various data

--- src/data.js
+++ src/data.js
@@ -8,17 +8,76 @@ var player2Avatars = [
 
 ];
 
+// Traditional game config object
+configTraditional = {
+  "rock": ["scissors"],
+  "scissors": ["paper"],
+  "paper": ["rock"],
+};
+
+// Extreme game config array
+// Merge with configTraditional object
+// https://webinuse.com/how-to-merge-objects-in-javascript/
+configExtreme = {
+  "one punch man": ["rock", "paper", "scissors", "goku", "alien"],
+  "goku": ["rock", "paper", "scissors", "alien", "mosquito"],
+  "alien": ["rock", "paper", "scissors", "mosquito"],
+  "mosquito": ["one punch man"],
+};
+
+
 // Strings
 var gameWinQuotes = [
-
+  "You Win!",
+  "Nice Fight!",
+  "Good Fight!",
+  "Well Done!",
+  "Very Good!",
+  "You're Awesome!",
+  "Noice!",
+  "Human Wins!",
+  "Flawless Victory"
+  "Woo Hoo!",
+  "It's Morbin Time!",
 ];
 
 // Strings
 var gameLossQuotes = [
-
+  "Outsmarted.",
+  "Better luck next time.",
+  "Outplayed.",
+  "Skynet Wins.",
+  "Fatality.",
+  "You Lose.",
+  "Oof.",
+  "Rekt.",
+  "Doh!",
+  "Not This Time."
+  "You got Morbed On",
 ];
 
 // Strings
 var gameTieQuotes = [
+  "Draw Game!",
+  "It's a Draw!",
+  "Great Minds Think Alike",
+  "What are the Odds?",
+  "You?? Me???",
+  "It's like looking in a Mirror ...",
+];
 
+var opmWinQuotes = [
+  "One PUUUUUUUUNCHED!!!"
+];
+
+var gokuWinQuotes = [
+  "Ka...me...ha...me...HAAAAAAAA!"
+];
+
+var alienWinQuotes = [
+  "Sleoncirfndk (You lose)."
+];
+
+var mosquitoWinQuotes = [
+  "BzzZzZZzzzZZZzzzZ."
 ];
