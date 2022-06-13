5fb20fe30d5369897575387f242f60c0aafd6c36
Anthony Shellman <atshellman@gmail.com>
2022-06-13 17:29:55 -0600

    Add various variables and potential functions

---
+++ src/main.js
@@ -0,0 +1,57 @@
+// QuerySelectors
+var sidebarLeft = document.getElementById('sideBarLeft');
+var gameSelect = document.getElementById('gameSelect');
+var gameoptions = document.getElementById('gameOptions');
+var sidebarRight = document.getElementById('sidebarRight');
+var changeGameButton = document.getElementById('changeGameButton');
+var resetScoresButton = document.getElementById('resetScoresButton');
+var gameModeButton = document.querySelector('.game-mode-button');
+var gameModeButton2 = document.querySelector('.game-mode-button2');
+
+// Event Listeners
+
+
+
+
+
+// Functions
+function newGame() {
+var humanPlayer = new Game();
+var robotPlayer = new Game()
+
+}
+
+function avatarSelect() {
+
+}
+
+function displayUpdatedAvatar() {
+
+}
+
+function gameSelect() {
+
+}
+function gameSetup() {
+
+}
+
+function turnTaken() {
+
+}
+
+function displayOutcome() {
+
+}
+
+function displayUpdatedScores() {
+
+}
+
+function changeGame() {
+
+}
+
+function resetScores() {
+
+}
