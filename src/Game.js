f1e2a249ddd69248061fd2dcea63680c5ab43de7
Anthony Shellman <atshellman@gmail.com>
2022-06-15 15:14:11 -0600

    Fix bug - images now display on win screen

--- src/Game.js
+++ src/Game.js
@@ -33,24 +33,30 @@ class Game {
     customQuote.innerText = `Human and Computer picked ${player1Result}: ` + drawQuote
     drawDisplayPlayer1.innerText = `Draws: ${this.players[0].stats.draws}`
     drawDisplayPlayer2.innerText = `Draws: ${this.players[1].stats.draws}`
-    player1Image.innerText = `src="./assets/${player1Result}.png" alt="${player1Result} avatar"`
-    player2Image.innerText = `src="./assets/${player2Result}.png" alt="${player2Result} avatar"`
+    gameResultView.innerHTML = `
+    <img class="resize-img player1-image" src="./assets/${player1Result}.png" alt="${player1Result} avatar">
+    <img class="resize-img player2-image" src="./assets/${player2Result}.png" alt="${player2Result} avatar">
+    `;
   } else if (this.rules[player2Result].includes(player1Result)) {
     this.players[0].stats.losses++
     this.players[1].stats.wins++
     customQuote.innerText = `Computer picked ${player2Result}: ` + loseQuote
     lossDisplayPlayer1.innerText = `Losses: ${this.players[0].stats.losses}`
     winDisplayPlayer2.innerText = `Wins: ${this.players[1].stats.wins}`
-    player1Image.innerText = `src="./assets/${player1Result}.png" alt="${player1Result} avatar"`
-    player2Image.innerText = `src="./assets/${player2Result}.png" alt="${player2Result} avatar"`
+    gameResultView.innerHTML = `
+    <img class="resize-img player1-image" src="./assets/${player1Result}.png" alt="${player1Result} avatar">
+    <img class="resize-img player2-image" src="./assets/${player2Result}.png" alt="${player2Result} avatar">
+    `;
   } else {
     this.players[0].stats.wins++
     this.players[1].stats.losses++
     customQuote.innerText = `You picked ${player1Result}: ` + winQuote
     winDisplayPlayer1.innerText = `Wins: ${this.players[0].stats.wins}`
     lossDisplayPlayer2.innerText = `Losses: ${this.players[1].stats.losses}`
-    player1Image.innerText = `src="./assets/${player1Result}.png" alt="${player1Result} avatar"`
-    player2Image.innerText = `src="./assets/${player2Result}.png" alt="${player2Result} avatar"`
+    gameResultView.innerHTML = `
+    <img class="resize-img player1-image" src="./assets/${player1Result}.png" alt="${player1Result} avatar">
+    <img class="resize-img player2-image" src="./assets/${player2Result}.png" alt="${player2Result} avatar">
+    `;
     }
   }
 
