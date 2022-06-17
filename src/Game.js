11679af0fe92bd4625fedf736f45f2a2aafbbecd
Anthony Shellman <atshellman@gmail.com>
2022-06-16 18:22:06 -0600

    Quality of life fixes, code restructuring

--- src/Game.js
+++ src/Game.js
@@ -29,18 +29,18 @@ class Game {
 
   if (player1Result === player2Result) {
     this.players[0].stats.draws ++;
-    this.players[1].stats.draws ++ ;
-    customQuote.innerText = `You and Computer picked ${player1Result}: ` + drawQuote
-    drawDisplayPlayer1.innerText = `Draws: ${this.players[0].stats.draws}`
-    drawDisplayPlayer2.innerText = `Draws: ${this.players[1].stats.draws}`
+    this.players[1].stats.draws ++;
+    customQuote.innerText = `You and Robot picked ${player1Result}: ` + drawQuote ;
+    drawDisplayPlayer1.innerText = `Draws: ${this.players[0].stats.draws}`;
+    drawDisplayPlayer2.innerText = `Draws: ${this.players[1].stats.draws}`;
     gameResultView.innerHTML = `
     <img class="resize-img player1-image" src="./assets/${player1Result}.png" alt="${player1Result} avatar">
     <img class="resize-img player2-image" src="./assets/${player2Result}.png" alt="${player2Result} avatar">
     `;
   } else if (this.rules[player2Result].includes(player1Result)) {
-    this.players[0].stats.losses++
-    this.players[1].stats.wins++
-    customQuote.innerText = `Computer picked ${player2Result}: ` + loseQuote
+    this.players[0].stats.losses ++;
+    this.players[1].stats.wins ++;
+    customQuote.innerText = `Robot picked ${player2Result}: ` + loseQuote
     lossDisplayPlayer1.innerText = `Losses: ${this.players[0].stats.losses}`
     winDisplayPlayer2.innerText = `Wins: ${this.players[1].stats.wins}`
     gameResultView.innerHTML = `
@@ -48,8 +48,8 @@ class Game {
     <img class="resize-img player2-image" src="./assets/${player2Result}.png" alt="${player2Result} avatar">
     `;
   } else {
-    this.players[0].stats.wins++
-    this.players[1].stats.losses++
+    this.players[0].stats.wins ++;
+    this.players[1].stats.losses ++;
     customQuote.innerText = `You picked ${player1Result}: ` + winQuote
     winDisplayPlayer1.innerText = `Wins: ${this.players[0].stats.wins}`
     lossDisplayPlayer2.innerText = `Losses: ${this.players[1].stats.losses}`
