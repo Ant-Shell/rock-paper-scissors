9e1509aa1af9a0b90fe438c130175b069205cf5c
Anthony Shellman <atshellman@gmail.com>
2022-06-20 15:48:19 -0600

    Remove un-needed method humanTakeTurn

--- src/Player.js
+++ src/Player.js
@@ -8,25 +8,6 @@ class Player {
     }
   }
 
-  humanTakeTurn(selection) {
-    var player1Result = selection;
-    if (player1Result === "rock") {
-      return "rock";
-    } else if (player1Result === "scissors") {
-      return "scissors";
-    } else if (player1Result === "paper") {
-    return "paper";
-  } else if (player1Result === "onepunchman") {
-    return "onepunchman";
-  } else if (player1Result === "goku") {
-    return "goku";
-  } else if (player1Result === "alien") {
-    return "alien";
-  } else if (player1Result === "mosquito") {
-    return "mosquito";
-  }
-}
-
   takeTurn(configObj) {
     var keys = Object.keys(configObj);
     return keys[Math.floor(Math.random() * keys.length)];
